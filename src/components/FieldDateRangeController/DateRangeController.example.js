83e437344bcbe730d0b0e19457a2909881b83d75
Jenni Nurmi <jenni@sharetribe.com>
2018-11-19 15:08:20 +0200

    Create DateRangeController component

---
+++ src/components/FieldDateRangeController/DateRangeController.example.js
@@ -0,0 +1,9 @@
+import DateRangeController from './DateRangeController';
+
+//const id = 'DateRangeControllerExample';
+
+export const DateRangeControllerExample = {
+  component: DateRangeController,
+  props: {},
+  group: 'custom inputs',
+};
